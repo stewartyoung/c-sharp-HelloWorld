7e94f6c7b522a7fa4ee61f854fcc8c469188b490
Stewart Young <stewart@dresser.app>
2019-06-03 20:15:36 +0100

    working code prints hello world to command line

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
-
+﻿// A Hello World! program in C#.
+using System;
 namespace HelloWorld
 {
-    class Program
+    class Hello
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
